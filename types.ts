b7c7947b205ef3dddb17ecfaae5ad9a5e9ce0824
laslhr3942 <laslhr3942@gmail.com>
2026-02-09 15:31:06 +0900

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,10 +1,13 @@
-
 export interface ProductInfo {
+  userName: string;
   productName: string;
   region: string;
   features: string;
+  producerStory: string; 
+  season: string;        
   target: string;
   tone: string;
+  outputFormat: 'SNS' | 'BLOG'; 
 }
 
 export interface GeneratedCopy {
